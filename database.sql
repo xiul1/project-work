6738b917b4b8b9e3ae1d39316874b63f82fe6a0d
xiul1 <gaydesabi@gmail.com>
2026-01-23 17:38:07 +0100

    Add initial database schema for KeyManager

---
+++ database.sql
@@ -0,0 +1,27 @@
+-- Active: 1769184600561@@127.0.0.1@3306@KeyManager
+CREATE DATABASE IF NOT EXISTS KeyManager;
+CREATE TABLE users(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    username VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    password_hash_master VARCHAR(255) NOT NULL,
+    salt VARCHAR(255) NOT NULL,
+    last_login TIMESTAMP NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+CREATE TABLE credenziali (
+    credential_id INT PRIMARY KEY AUTO_INCREMENT,
+    user_id INT(11),
+    FOREIGN KEY (user_id) REFERENCES users(id)
+        ON DELETE CASCADE 
+        ON UPDATE CASCADE,
+    service_name VARCHAR(255) NOT NULL,
+    username VARCHAR(255) NOT NULL,
+    password_encrypted TEXT NOT NULL,
+    url VARCHAR(255),
+    notes TEXT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP        
+);
+
+
