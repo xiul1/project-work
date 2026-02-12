720e5b321a4cdc91f6c6aea718fd2956e6ebebd4
xiul1 <gaydesabi@gmail.com>
2026-02-12 19:07:56 +0100

    Refactor code structure for improved readability and maintainability

--- register.php
+++ register.php
@@ -1,4 +1,7 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 require "pdo.php";  // Include la connessione al DB
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
